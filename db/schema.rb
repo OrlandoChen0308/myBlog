14c1667168a5b12453460e1b8ee7eb8761839b8f
OrlandoChen0308 <orlandochen0308@gmail.com>
2015-10-12 16:46:12 -0500

    Recover to Import version

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20151009014303) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "start_time"
+    t.datetime "start_time" 
     t.datetime "end_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
