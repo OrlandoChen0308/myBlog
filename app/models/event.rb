b3ce5ddce363f8601ba0a7fbfc4d11ef4f1b0a14
OrlandoChen0308 <orlandochen0308@gmail.com>
2015-10-13 10:23:46 -0500

    Finish Up All Function, Yeah

--- app/models/event.rb
+++ app/models/event.rb
@@ -19,6 +19,26 @@ def self.import(file)
   end
 end
 
+# def self.import(file)
+#   spreadsheet = open_spreadsheet(file)
+#   header = spreadsheet.row(1)
+#   (2..spreadsheet.last_row).each do |i|
+#     row = Hash[[header, spreadsheet.row(i)].transpose]
+#     event = find_by_id(row["id"]) || new
+#     event.attributes = row.to_hash.slice(*accessible_attributes)
+#     event.save!
+#   end
+# end
+
+# def self.open_spreadsheet(file)
+#   case File.extname(file.original_filename)
+#   when ".csv" then Csv.new(file.path, nil, :ignore)
+#   when ".xls" then Excel.new(file.path, nil, :ignore)
+#   when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+#   else raise "Unknown file type: #{file.original_filename}"
+#   end
+# end
+
 	private
 	def build_default_stat
   # build default profile instance. Will use default params.
